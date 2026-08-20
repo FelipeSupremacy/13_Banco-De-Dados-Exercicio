870534c069bc4f5762e2a12a2c2db518290c79f0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-20 09:54:36 -0300

    Continuacao do banco de dados com inserir

--- src/main/java/dao/VendedorDAO.java
+++ src/main/java/dao/VendedorDAO.java
@@ -3,16 +3,14 @@ package dao;
 import factory.ConnectionFactory;
 import model.Vendedor;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.SQLOutput;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
     @Override
     public void inserir(Vendedor entidade) {
-        String sql = "Insert nito java_vendedor(nome) values(?)";
+        String sql = "Insert into java_vendedor(nome) values(?)";
 
         try(Connection connection = ConnectionFactory.obterConexao();
             PreparedStatement ps = connection.prepareStatement(sql)) {
@@ -26,6 +24,21 @@ public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
 
     @Override
     public List<Vendedor> listar() {
-        return List.of();
+        List<Vendedor>lista = new ArrayList<>();
+        String sql = "select * from java_vendedor";
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareCall(sql);
+            ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Vendedor vendedor = new Vendedor();
+                vendedor.setId(rs.getInt("id"));
+                vendedor.setNome(rs.getString("Nome"));
+                lista.add(vendedor);
+            }
+        }
+        catch(SQLException e){
+
+        }
+        return lista;
     }
 }
